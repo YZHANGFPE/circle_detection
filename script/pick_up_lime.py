0af26a50d3161b44e0507918070020e1ad35bb17
ArcLab 02 <umdarclab@gmail.com>
2015-08-04 10:44:16 -0400

    test set orientation constraint

--- script/pick_up_lime.py
+++ script/pick_up_lime.py
@@ -3,6 +3,7 @@ import rospy
 import tf
 import numpy
 import moveit_commander
+import moveit_msgs.msg
 
 from geometry_msgs.msg import Pose, Point, Quaternion
 
@@ -11,25 +12,20 @@ if __name__ == '__main__':
 
     tf_listener = tf.TransformListener()
 
-    # group = moveit_commander.MoveGroupCommander("left_arm")
-    # group.set_planner_id('RRTConnectkConfigDefault')
+    group = moveit_commander.MoveGroupCommander("left_arm")
+    group.set_planner_id('RRTConnectkConfigDefault')
 
-    # # initial pose
-    # initial_pose = Pose()
-    # initial_pose.orientation.y = 1.0
-    # initial_pose.position.x = 0.6
-    # initial_pose.position.y = 0.48
-    # initial_pose.position.z = 0.144
+    # initial pose
+    initial_pose = Pose()
+    initial_pose.orientation.y = 1.0
+    initial_pose.position.x = 0.6
+    initial_pose.position.y = 0.48
+    initial_pose.position.z = 0.144
 
-    # group.set_pose_target(initial_pose)
-    # group.go()
+    group.set_pose_target(initial_pose)
+    plan1 = group.plan()
+    group.execute(plan1)
 
-    # pose_target = geometry_msgs.msg.Pose()
-    # pose_target.orientation.y = 1.0
-    # pose_target.position.x = 0.7
-    # pose_target.position.y = -0.05
-    # pose_target.position.z = 1.1
-    # group.set_pose_target(pose_target)
     rospy.loginfo("Looking for the detected pose")
 
     while not rospy.is_shutdown():
@@ -42,20 +38,50 @@ if __name__ == '__main__':
 
     rospy.loginfo(trans)
 
+    # move to the position that is above the lime
+
     above_pose = Pose()
     above_pose.orientation.y = 1.0
     above_pose.position.x = trans[0]
     above_pose.position.y = trans[1]
     above_pose.position.z = 0.144
     group.set_pose_target(above_pose)
-    group.go()
+    plan2 = group.plan()
+    group.execute(plan2)
+
+    # set the start state as the end of the last plan
+    # state = moveit_msgs.msg.RobotState()
+    # state.joint_state.header = above_plan.joint_trajectory.header
+    # state.joint_state.name = above_plan.joint_trajectory.joint_names
+    # state.joint_state.position = above_plan.joint_trajectory.points[-1].positions
+    # state.joint_state.velocity = above_plan.joint_trajectory.points[-1].velocities
+    # state.joint_state.effort = above_plan.joint_trajectory.points[-1].effort
+
+    # state.multi_dof_joint_state.header = above_plan.multi_dof_joint_trajectory.header
+    # state.multi_dof_joint_state.joint_names = above_plan.multi_dof_joint_trajectory.joint_names
+
+    # group.set_start_state(state)
+
+    # set the orientation constraint during the approach
+    constraints = moveit_commander.Constraints()
+    constraints.name = "hold rotations"
+    ocm = moveit_msgs.msg.OrientationConstraint()
+    ocm.link_name = "left_hand"
+    ocm.header.frame_id = "base"
+    ocm.orientation = above_pose.orientation
+    ocm.absolute_x_axis_tolerance = 100
+    ocm.absolute_y_axis_tolerance = 100
+    ocm.absolute_z_axis_tolerance = 100
+    ocm.weight = 1.0
+
+    constraints.orientation_constraints.append(ocm)
+
+    group.set_path_constraints(constraints)
 
     pick_pose = Pose()
     pick_pose.orientation.y = 1.0
     pick_pose.position.x = trans[0]
     pick_pose.position.y = trans[1]
-    pick_pose.position.z = -0.03
+    pick_pose.position.z = -0.08
     group.set_pose_target(pick_pose)
-    group.go()
-
-    rospy.sleep(2)
+    group.plan()
