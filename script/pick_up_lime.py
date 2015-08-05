eec0707c61ca9d4b19e13639350a0a979a7b6611
ArcLab 02 <umdarclab@gmail.com>
2015-08-05 11:46:12 -0400

    add logic to ignore the detection 2.0 sec older

--- script/pick_up_lime.py
+++ script/pick_up_lime.py
@@ -6,6 +6,7 @@ import moveit_commander
 import moveit_msgs.msg
 import sys
 from geometry_msgs.msg import Pose, Point, Quaternion
+from baxter_interface import Gripper
 
 if __name__ == '__main__':
     moveit_commander.roscpp_initialize(sys.argv)
@@ -15,82 +16,138 @@ if __name__ == '__main__':
 
     robot = moveit_commander.RobotCommander()
     scene = moveit_commander.PlanningSceneInterface()
+    gripper = Gripper("left")
+    gripper.calibrate()
+    rospy.sleep(2)
 
     group = moveit_commander.MoveGroupCommander("left_arm")
     group.set_planner_id('RRTConnectkConfigDefault')
 
-    # initial pose
-    initial_pose = Pose()
-    initial_pose.orientation.y = 1.0
-    initial_pose.position.x = 0.6
-    initial_pose.position.y = 0.48
-    initial_pose.position.z = 0.144
+    no_detection = False
 
-    group.set_pose_target(initial_pose)
-    plan1 = group.plan()
-    group.execute(plan1)
 
-    rospy.loginfo("Looking for the detected pose")
+    # # initial pose
+    # initial_pose = group.get_current_pose()
 
-    while not rospy.is_shutdown():
-        try:
-            tf_listener.waitForTransform('/base', '/circle', rospy.Time(0), rospy.Duration(4.0))
-            break
-        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-            continue
-    trans, rot = tf_listener.lookupTransform('/base', '/circle', rospy.Time(0))
+    # rospy.sleep(2)
 
-    rospy.loginfo(trans)
+    # # press pose
+    # press_pose = group.get_current_pose()
+    # press_pose.pose.position.z -= 0.06
+    # group.set_pose_target(press_pose)
+    # group.go()
+    # rospy.sleep(5)
 
-    # move to the position that is above the lime
-    above_pose = Pose()
-    above_pose.orientation.y = 1.0
-    above_pose.position.x = trans[0]
-    above_pose.position.y = trans[1]
-    above_pose.position.z = 0.144
-    group.set_pose_target(above_pose)
-    plan2 = group.plan()
-    group.execute(plan2)
+    # group.set_pose_target(initial_pose)
+    # group.go()
 
-    # set the start state as the end of the last plan
-    # state = moveit_msgs.msg.RobotState()
-    # state.joint_state.header = above_plan.joint_trajectory.header
-    # state.joint_state.name = above_plan.joint_trajectory.joint_names
-    # state.joint_state.position = above_plan.joint_trajectory.points[-1].positions
-    # state.joint_state.velocity = above_plan.joint_trajectory.points[-1].velocities
-    # state.joint_state.effort = above_plan.joint_trajectory.points[-1].effort
+    while not no_detection:
 
-    # state.multi_dof_joint_state.header = above_plan.multi_dof_joint_trajectory.header
-    # state.multi_dof_joint_state.joint_names = above_plan.multi_dof_joint_trajectory.joint_names
+        # initial pose
+        initial_pose = Pose()
+        initial_pose.orientation.y = 1.0
+        initial_pose.position.x = 0.6
+        initial_pose.position.y = 0.48
+        initial_pose.position.z = 0.144
 
-    # group.set_start_state(state)
+        group.set_pose_target(initial_pose)
+        group.go()
 
-    # set the orientation constraint during the approach
+        rospy.loginfo("Time to kill the process")
 
-    above_pose = group.get_current_pose()
+        start_time = rospy.get_time()
+        while (rospy.get_time() - start_time < 4.0):
+            if rospy.is_shutdown():
+                no_detection = True
+                break
 
-    pick_pose = Pose()
-    pick_pose.orientation = above_pose.pose.orientation
-    pick_pose.position.x = trans[0]
-    pick_pose.position.y = trans[1]
-    pick_pose.position.z = -0.04
+        rospy.loginfo("Looking for the detected pose")
 
-    group.set_pose_target(pick_pose)
 
-    constraints = moveit_commander.Constraints()
-    constraints.name = "hold rotations"
-    ocm = moveit_msgs.msg.OrientationConstraint()
-    ocm.link_name = "left_hand"
-    ocm.header.frame_id = "base"
-    ocm.orientation = pick_pose.orientation
-    ocm.absolute_x_axis_tolerance = 0.1
-    ocm.absolute_y_axis_tolerance = 0.1
-    ocm.absolute_z_axis_tolerance = 0.1
-    ocm.weight = 1.0
-    
-    constraints.orientation_constraints.append(ocm)
-    group.clear_path_constraints()
+        start_time = rospy.get_time()
 
-    group.set_path_constraints(constraints)
+        while not rospy.is_shutdown():
 
-    group.plan()
+            if(rospy.get_time() - start_time > 4.0):
+                no_detection = True
+                break
+            try:
+                tf_listener.waitForTransform('/base', '/circle', rospy.Time(0), rospy.Duration(1.0))
+                break
+            except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+                continue
+
+        if not no_detection:
+            time_delay = rospy.get_time() - tf_listener.getLatestCommonTime('/circle' , '/base').to_sec()
+            rospy.loginfo("The time delay for the detection is: %f sec" , time_delay)
+            if(time_delay > 3.0):
+                rospy.loginfo("Time delay of the detection exceeds 2.0 second. Stop the process")
+                break
+
+            trans, rot = tf_listener.lookupTransform('/base', '/circle', rospy.Time(0))
+
+            rospy.loginfo(trans)
+
+            # move to the position that is above the lime
+            above_pose = Pose()
+            above_pose.orientation.y = 1.0
+            above_pose.position.x = trans[0]
+            above_pose.position.y = trans[1]
+            above_pose.position.z = 0.144
+            group.set_pose_target(above_pose)
+            group.go()
+
+            # clean the scene
+            moveit_commander.PlanningSceneInterface().remove_world_object("table")
+
+
+            # set the orientation constraint during the approach
+
+            above_pose = group.get_current_pose()
+
+
+            pick_pose = Pose()
+            pick_pose.orientation = above_pose.pose.orientation
+            pick_pose.position.x = trans[0]
+            pick_pose.position.y = trans[1]
+            pick_pose.position.z = -0.08
+
+            group.set_pose_target(pick_pose)
+
+            constraints = moveit_commander.Constraints()
+            constraints.name = "hold rotations"
+            ocm = moveit_msgs.msg.OrientationConstraint()
+            ocm.link_name = "left_hand"
+            ocm.header.frame_id = "base"
+            ocm.orientation = pick_pose.orientation
+            ocm.absolute_x_axis_tolerance = 0.1
+            ocm.absolute_y_axis_tolerance = 0.1
+            ocm.absolute_z_axis_tolerance = 0.1
+            ocm.weight = 1.0
+            
+            constraints.orientation_constraints.append(ocm)
+            group.clear_path_constraints()
+
+            group.set_path_constraints(constraints)
+
+            group.go()
+            group.clear_path_constraints()
+            gripper.close()
+            rospy.sleep(1)
+
+            # fake motion
+
+            initial_pose = group.get_current_pose()
+
+            press_pose = group.get_current_pose()
+            press_pose.pose.position.z += 0.06
+            group.set_pose_target(press_pose)
+            group.go()
+
+            group.set_pose_target(initial_pose)
+            group.go()
+
+            gripper.open()
+            rospy.sleep(1)
+
+    rospy.loginfo("No circle detection")
